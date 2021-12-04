da7b2cf57cd482407de5fe3bce22f8850f7fd1ff
SHEIKH MUHAMMAD SAAD <81197058+SHEIKHSAAD2561@users.noreply.github.com>
2021-12-04 14:00:32 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! This is your Boy Saad Here");
+
+        System.out.println("My Name is Saad and i am student of Fast National University Islamabad");
 	    
     }
 }
